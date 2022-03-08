73c3aeacde0ac18f5832a6674ad13e8569d5776c
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-08 14:52:04 +0000

    Done !

---
+++ main.js
@@ -0,0 +1,7 @@
+function risposta() {
+
+let name = prompt("Chi sei ! Inserisci il tuo nome!");
+let secondName = prompt("inserisci subito il tuo cognome!");
+let color = prompt("Dimmi il tuo colore preferito!");
+  
+document.getElementById("risultato").innerHTML =  "Ciao " + name + " " + secondName + " " + "Il tuo colore preferito è" + " " + color;}
